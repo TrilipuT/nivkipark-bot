337819814e757cbc904a5e9dd7594eaddf02c36e
Vitaly Nikolaiev <vitaly.nikolaiev@datarobot.com>
2024-03-31 13:18:49 +0300

    add blocking users

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,16 +1,40 @@
 import {Composer, Keyboard} from "grammy";
 import {MyContext} from "../index";
+import {backToStart} from "./menu";
+
+const blocked = {
+    "b1": [],
+    "b2": [],
+    "b3": [],
+    "b4": [],
+    "b5": [],
+    "b6": ['2', '20', '24', '33', '41', '60', '63', /*'65',*/ '78', '83', '89', '96', '110', '141', '150', '168', '178', '182', '185', '191', '211', '213'],
+    "b7": [],
+    "b8": [],
+    "b9": [],
+    "b10": [],
+    "b11": [],
+    "b12": [],
+}
 
 const bot = new Composer<MyContext>();
-export async function isAuthenticated(ctx:MyContext) {
+
+export async function isAuthenticated(ctx: MyContext) {
     await ctx.session
-    const isAuth = ctx.session.contact.phone_number && await ctx.session.building && await ctx.session.flat
+    let allow = true
+    const isAuth = ctx.session.contact.phone_number && ctx.session.building && ctx.session.flat
     if (!isAuth) {
-        ctx.reply('Щось я вас не впізнаю... Пройдіть авторизаію заново, будь ласка', {
+        allow = false
+        await ctx.reply('Щось я вас не впізнаю... Пройдіть авторизацію заново, будь ласка 👇', {
             reply_markup: new Keyboard().text('/auth').resized().oneTime()
         })
     }
-    return isAuth
+    const isBlocked = blocked[ctx.session.building] && blocked[ctx.session.building].includes(ctx.session.flat)
+    if (isBlocked) {
+        allow = false
+        await backToStart(ctx, '❗️Користування ботом обмежено.❗️\nПеревірте наявність заборгованості перед ОСББ/ЖЕК.\nДля детальної інформації зверніться до представника вашої управляючої компанії. ')
+    }
+    return allow
 }
 
 bot.command("auth", async (ctx) => {
